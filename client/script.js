5d6e7ef242a0ec290dd7702cb8df93520f54394e
rntxgt <lucasquaresm1@gmail.com>
2026-01-22 22:53:22 -0300

    prototipo de conexão `client/script.js` e `server/server.js`

--- client/script.js
+++ client/script.js
@@ -1,1 +1,12 @@
+// Usa a API nativa do navegador
+const socket = new WebSocket("ws://localhost:8080");
+const nome = prompt("Insira seu nome: ");
 
+// Quando recebe mensagem
+socket.onmessage = (message) => {
+  console.log(message.data);
+};
+
+function sendMessage(message) {
+  socket.send(nome + ": " + message);
+}
---
+++ server/server.js
@@ -0,0 +1,25 @@
+const WebSocket = require("ws");
+// Cria um servidor na porta 8080
+const wss = new WebSocket.Server({ port: 8080 });
+
+console.log("Servidor rodando na porta 8080...");
+
+wss.on("connection", (ws) => {
+  console.log(`Cliente conectou!`);
+
+  // Evento: quando chega mensagem do cliente
+  ws.on("message", (message) => {
+    console.log("Recebido:", message.toString());
+
+    // Broadcast: Envia a mensagem para todos os clientes conectados
+    wss.clients.forEach((client) => {
+      // Verifica se o cliente está com a conexão aberta antes de enviar
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+
+  // Envia uma mensagem de boas-vindas assim que conecta
+  ws.send("Bem-vindo ao servidor do CriptChat!");
+});
